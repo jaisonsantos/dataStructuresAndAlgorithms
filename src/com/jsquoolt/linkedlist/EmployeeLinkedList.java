1965c511622901bf83f85bb08b7d931084043fc8
Jaison Santos <jaison_js@hotmail.com>
2020-08-12 23:17:57 +0200

    Linked List  - implemented method to addBefore

--- src/com/jsquoolt/linkedlist/EmployeeLinkedList.java
+++ src/com/jsquoolt/linkedlist/EmployeeLinkedList.java
@@ -82,6 +82,42 @@ public class EmployeeLinkedList {
         return removedNode;
     }
 
+    public boolean addBefore(Employee newEmployee, Employee existingEmployee) {
+
+        // return true if you were able to successfully add the employee
+        // into the list before the existing employee.
+        // Return false if the existing employee doesn't exist in the list
+        if (head == null) {
+            return false;
+        }
+
+        // find the existing employee
+        EmployeeNode current = head;
+        while (current != null && !current.getEmployee().equals(existingEmployee)) {
+            current = current.getNext();
+        }
+
+        if (current == null) {
+            return false;
+        }
+
+        EmployeeNode newNode = new EmployeeNode(newEmployee);
+        newNode.setPrevious(current.getPrevious());
+        newNode.setNext(current);
+        current.setPrevious(newNode);
+
+        if (head == current) {
+            head = newNode;
+        }
+        else {
+            newNode.getPrevious().setNext(newNode);
+        }
+
+        size++;
+
+        return true;
+    }
+
     public void printList() {
         EmployeeNode current = head;
         System.out.print("HEAD -> ");
--- src/com/jsquoolt/linkedlist/Main.java
+++ src/com/jsquoolt/linkedlist/Main.java
@@ -5,9 +5,10 @@ public class Main {
     public static void main(String[] args) {
 
         Employee janeJones = new Employee("Jane", "Jones", 123);
-        Employee johnDoe = new Employee("John", "Doe", 123);
-        Employee marySmith = new Employee("Mary", "Smith", 123);
-        Employee mikeWilson = new Employee("Mike", "Wilson", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+        Employee billyMand = new Employee("Billy", "Mand", 89);
 
         EmployeeLinkedList list = new EmployeeLinkedList();
 
@@ -18,6 +19,11 @@ public class Main {
         list.addToFront(johnDoe);
         list.addToFront(marySmith);
         list.addToFront(mikeWilson);
+        list.printList();
+
+
+        list.addBefore(billyMand, mikeWilson);
+        list.printList();
 
         System.out.println(list.getSize());
 
